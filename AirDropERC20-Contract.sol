a278e00077d66f4b217fb4eb7a055f0c4b1d6c81
Net2Dev <info@a3b.io>
2022-06-12 15:06:01 -0700

    Update AirDropERC20-Contract.sol

--- AirDropERC20-Contract.sol
+++ AirDropERC20-Contract.sol
@@ -1,1 +1,21 @@
 
+// SPDX-License-Identifier: MIT LICENSE
+import "https://github.com/net2devcrypto/Simple-ERC20-AirdropServer/N2DPlus.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+pragma solidity ^0.8.4;
+
+contract AirDropNFT is Ownable  {
+    
+    uint256 public airDrop = 10 ether;
+    N2DPlus rewards;
+
+    constructor(N2DPlus _rewards) {
+        rewards = _rewards;
+    }
+
+    function issueAirDropERC20(address[] calldata holder) public onlyOwner {
+        rewards.mintAirdrop(holder, airDrop);
+    }
+
+}
